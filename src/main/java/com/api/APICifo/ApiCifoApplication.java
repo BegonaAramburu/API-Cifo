b6961171752ef01c676a023f38192d84d0b5968e
BegonaAramburu <begona.aramburu@gmail.com>
2019-12-22 11:55:42 +0100

    base

---
+++ src/main/java/com/api/APICifo/ApiCifoApplication.java
@@ -0,0 +1,13 @@
+package com.api.APICifo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCifoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCifoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/APICifo/ApiCifoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.APICifo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCifoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
