4f03991d00643965a89270e86f441b9bb096cac7
BegonaAramburu <begona.aramburu@gmail.com>
2020-01-14 17:20:48 +0100

    commit F5

--- src/main/java/com/api/APICifo/controllers/CourseController.java
+++ src/main/java/com/api/APICifo/controllers/CourseController.java
@@ -3,11 +3,13 @@ package com.api.APICifo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.APICifo.domains.Course;
+import com.api.APICifo.domains.Offer;
 import com.api.APICifo.services.CourseService;
 
 @RestController
@@ -19,6 +21,12 @@ public class CourseController {
 	@GetMapping("/api.cifo/areas/{id}/cursos")
 	public List<Course> getCoursesByAreaId(@PathVariable Integer id){
 		return courseService.findByAreaId(id);
+	}
+	
+	//Get offers by chars
+	@GetMapping("/api.cifo/cursos/{name}")
+	public List<Course> getOffersByChars(@PathVariable String name){
+		return courseService.getCoursesByChars(name);
 	}
 
 }
